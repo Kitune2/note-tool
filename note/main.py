bb4881307705a2af9d550f77cb4ac47f33a1a01b
kitune <xiaoyexiaoye63@gmail.com>
2023-11-12 20:34:02 +0900

    first

--- note/main.py
+++ note/main.py
@@ -8,12 +8,12 @@ class note:
 
     }
     def like(self,url,session_id=None):
-        self.header['Cookie']= f'_note_session_v5={session_id};'
-        article_id = url.split('/')[6]
-        XSRFtoken = session.post("https://note.com/api/v3/trackings/fp",headers=self.header).cookies.get("XSRF-TOKEN")
+        article_id = url.split('/')[-1]
         r = session.get("https://note.com/",headers=self.header)
         if session_id is None:
             session_id = r.cookies.get("_note_session_v5")
+        self.header['Cookie']= f'_note_session_v5={session_id};'
+        XSRFtoken = session.post("https://note.com/api/v3/trackings/fp",headers=self.header).cookies.get("XSRF-TOKEN")
         client_code = r.text.split('session')[1].split('"')[1]
         header = {
             "Accept":"application/json, text/plain, */*",
@@ -28,5 +28,29 @@ class note:
               "_note_session_v5":session_id,
               "XSRF-TOKEN":XSRFtoken
         }
-        r = session.post(f"https://note.com/api/v3/notes/{article_id}/likes",headers=self.header,cookies=cookie)
+        print(self.header)
+        r = session.post(f"https://note.com/api/v3/notes/{article_id}/likes",headers=header,cookies=cookie)
+        print(r.text)
+
+
+    def follow(self, session_id, user_name):
+        self.header['Cookie']= f'_note_session_v5={session_id};'
+        user_id = session.get(f"https://note.com/api/v2/creators/{user_name}",headers=self.header).json()["data"]["key"]
+        r = session.get("https://note.com/",headers=self.header)
+        XSRFtoken = session.post("https://note.com/api/v3/trackings/fp",headers=self.header).cookies.get("XSRF-TOKEN")
+        client_code = r.text.split('session')[1].split('"')[1]
+        header = {
+            "Accept":"application/json, text/plain, */*",
+            "Accept-Encoding":"gzip, deflate, br",
+            "Accept-Language":"ja,en-US;q=0.9,en;q=0.8",
+            "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100",
+            "X-Note-Client-Code":client_code,
+            "X-Requested-With":"XMLHttpRequest, XMLHttpRequest",
+            "X-Xsrf-Token":XSRFtoken
+        }
+        cookie = {
+              "_note_session_v5":session_id,
+              "XSRF-TOKEN":XSRFtoken
+        }
+        r = requests.post(f"https://note.com/api/v3/users/{user_id}/following",headers=header,cookies=cookie)
         print(r)
